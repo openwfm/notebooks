bc94acd8f85a42a685b95aecdc40a486207ee8bc
Jan <jan.mandel@gmail.com>
2023-04-10 00:40:23 -0600

    adding rnn_model_sequential and test

--- fmda/fullrnn.py
+++ fmda/fullrnn.py
@@ -48,14 +48,6 @@ def plain_python_full_simple_rnn(inputs, initial_state, kernel, bias):
     print('output.shape=(units,):',output.shape)
     return output
 
-def create_rnn_model(units, input_shape):
-    model = Sequential([
-        Input(input_shape),
-        FullSimpleRNN(units),
-    ])
-    return model
-
-
 def test_full_simple_rnn_functional_model():
     units = 60
     timesteps = 1
@@ -96,6 +88,59 @@ def test_full_simple_rnn_dims():
     output, _ = layer(inputs, initial_state)
     print(output.shape)
 
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, Model, Sequential
+from tensorflow.keras.layers import Input
+
+def rnn_model_sequential(time_steps, input_dim, rnn_units, output_units):
+    model = Sequential([
+        Input(shape=(time_steps, input_dim)),
+        FullSimpleRNN(units=rnn_units),
+        layers.Dense(output_units, activation='softmax')
+    ])
+    return model
+
+def manual_rnn_output(inputs, rnn_weights, rnn_bias, dense_weights, dense_bias):
+    initial_state = np.zeros((inputs.shape[0], rnn_units))
+    concatenated_inputs = np.concatenate([inputs, np.expand_dims(initial_state, 1)], axis=-1)
+    rnn_output = np.tanh(np.matmul(concatenated_inputs, rnn_weights) + rnn_bias)
+    
+    dense_input = rnn_output[:, -1, :]
+    dense_output = np.matmul(dense_input, dense_weights) + dense_bias
+    softmax_output = np.exp(dense_output) / np.sum(np.exp(dense_output), axis=1, keepdims=True)
+    
+    return softmax_output
+
+def create_and_test_model():
+    time_steps = 10
+    input_dim = 8
+    rnn_units = 32
+    output_units = 3
+
+    model = rnn_model_sequential(time_steps, input_dim, rnn_units, output_units)
+
+    # Generate random input data for testing
+    input_data = np.random.rand(1, time_steps, input_dim)
+
+    # Get the weights and biases from the model
+    rnn_weights, rnn_bias = model.layers[1].get_weights()
+    dense_weights, dense_bias = model.layers[2].get_weights()
+
+    # Compute the output using the model.predict method
+    model_output = model.predict(input_data)
+
+    # Compute the output using the manual_rnn_output function
+    manual_output = manual_rnn_output(input_data, rnn_weights, rnn_bias, dense_weights, dense_bias)
+
+    # Compare the outputs
+    print("Model output:\n", model_output)
+    print("Manual output:\n", manual_output)
+    print("Outputs are close:", np.allclose(model_output, manual_output))
+
+
+
 if __name__ == "__main__":
     #test_full_simple_rnn_dims()
     test_full_simple_rnn_functional_model()
+    create_and_test_model()
