7aa74fd40abb719dc32c78d54b834424b7793b7c
jh-206 <jhirschi206@gmail.com>
2023-05-24 22:54:51 -0600

    Rework paths and xarray load

--- jh-dev/data_funcs.py
+++ jh-dev/data_funcs.py
@@ -302,12 +302,14 @@ def raws_data(start=None, hours=None, h2=None, stid=None,meso_token=None):
 import os
 import subprocess
 
-def download_grib(source_url,time,model,dest_dir="",fmt = "%Y-%m-%d %H:%M"):
+def download_grib(source_url,time,model,dest_dir="",fmt = "%Y-%m-%d %H:%M",sector="conus",forecast_hour = 0  ):
     # source_url: starting url string that specifies grib data source
     # time: time of time slice to gather data
     # model: particular grib file to get from time slice
     # dest_dir: destination subdirectory, send grib file to this location
     # fmt: date format string
+    # sector: either continental us (conus) or alaska
+    # forecast_hour: offset from cycle time
     
     ## Utility function, lightweight so defined within this func
     ## source: chat-gpt4
@@ -317,21 +319,46 @@ def download_grib(source_url,time,model,dest_dir="",fmt = "%Y-%m-%d %H:%M"):
         else:
             raise AssertionError(f"The file '{file_path}' does not exist.")
 
+            
+            
+            
+    source_url = "https://noaa-hrrr-bdp-pds.s3.amazonaws.com"
+    # sector = "conus"
+    # start_time = "2022-06-01 00:00"
+    fmt = "%Y-%m-%d %H:%M"
+    time1 = datetime.strptime(time, fmt)
+    day_date=time1.strftime("%Y%m%d")
+    cycle = time1.hour          # hour
+    product = "wrfsfcf" # 2D surface levels
+
+    # Put it all together
+    file_path = f"hrrr.t{cycle:02}z.{product}{forecast_hour:02}.grib2"
+    grib_url=source_url+"/hrrr."+day_date+"/conus/"+file_path
+    
+    
+    
+    
+    
+    
     ## Construct full destination file with path
-    time_date = datetime.strptime(time, fmt)
-    day_date = time_date.strftime("%Y%m%d")
-    hourmin = time_date.strftime("%H%M")
-    dest_file=dest_dir+"/hrrr."+day_date+hourmin+".grib2"
-        
-    grib_url=source_url+day_date+"/conus/hrrr."+model+hourmin+".grib2"
+    # time_date = datetime.strptime(time, fmt)
+    # day_date = time_date.strftime("%Y%m%d")
+    # hour=time_date.strftime("%H")
+    # mins=time_date.strftime("%M")
+    dest_file=dest_dir+"/hrrr_"+day_date+str(cycle)+".grib2"
+
+    # grib_url=source_url+day_date+"/conus/hrrr.t"+hour+"z."+model+hour+".grib2"
     
     command = " ".join(["wget",grib_url, "-O",dest_file])
+    
+    print(command)
     
     subprocess.call(command,shell=True)
     
     # Assert T that file exists
     check_file_exists(dest_file)
     
+    return dest_file, grib_url
     
 def gather_hrrr_time_range(start, end, 
                            source_url = "https://noaa-hrrr-bdp-pds.s3.amazonaws.com/hrrr.",
