a9f9a9e7c9108e03686f69dd592f10c94c987203
Jan Mandel <jan.mandel@gmail.com>
2023-07-07 15:33:43 -0600

    by band, no single metadata item may determite the field uniquely

--- fmda/data/grib_to_geotiff.py
+++ fmda/data/grib_to_geotiff.py
@@ -2,53 +2,52 @@ import argparse
 from osgeo import gdal
 import numpy as np
 
-def grib_to_geotiff(input_filename, output_filename, variable_name):
+def grib_to_geotiff(input_filename, output_filename, band_number):
     # Open the GRIB file
     ds = gdal.Open(input_filename)
     
-    # Loop through all bands in the dataset and check their descriptions
-    for i in range(ds.RasterCount):
-        band = ds.GetRasterBand(i+1)  # 1-based index
-        metadata = band.GetMetadata()
+    # Get the specified band
+    band = ds.GetRasterBand(band_number)
+    metadata = band.GetMetadata()
 
-        # Check if the description matches the variable we're looking for
-        if metadata.get('GRIB_COMMENT') == variable_name:
-            # Read the band data
-            arr = band.ReadAsArray()
+    # Print the metadata
+    print(f"Metadata for band {band_number}:")
+    for key, value in metadata.items():
+        print(f"{key}: {value}")
 
-            # Create a new data source in memory
-            driver = gdal.GetDriverByName("GTiff")
-            out_ds = driver.Create(output_filename, band.XSize, band.YSize, 1, band.DataType)
+    # Check if the band data is 2D
+    arr = band.ReadAsArray()
+    if len(arr.shape) != 2:
+        print(f"Skipping band {band_number} because it is not 2D")
+        return
 
-            # Set the geotransform and projection
-            out_ds.SetGeoTransform(ds.GetGeoTransform())
-            out_ds.SetProjection(ds.GetProjection())
+    # Create a new data source in memory
+    driver = gdal.GetDriverByName("GTiff")
+    out_ds = driver.Create(output_filename, band.XSize, band.YSize, 1, band.DataType)
 
-            # Write the data to the new data source
-            out_band = out_ds.GetRasterBand(1)
-            out_band.WriteArray(arr)
+    # Set the geotransform and projection
+    out_ds.SetGeoTransform(ds.GetGeoTransform())
+    out_ds.SetProjection(ds.GetProjection())
 
-            # Close the data sources
-            ds = None
-            out_ds = None
+    # Write the data to the new data source
+    out_band = out_ds.GetRasterBand(1)
+    out_band.WriteArray(arr)
 
-            print(f"Variable {variable_name} from {input_filename} saved to {output_filename}")
-            return  # Exit the function
+    # Close the data sources
+    ds = None
+    out_ds = None
 
-    print(f"Variable {variable_name} not found in {input_filename}")
-
+    print(f"Band {band_number} from {input_filename} saved to {output_filename}")
 
 if __name__ == '__main__':
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Extract variables from a GRIB file and save as GeoTIFFs.')
+    parser = argparse.ArgumentParser(description='Extract a band from a GRIB file and save as a GeoTIFF if it is 2D.')
     parser.add_argument('input_filename', help='Path to the input GRIB file')
     parser.add_argument('output_filename', help='Path to the output GeoTIFF file')
-    parser.add_argument('variable_names', nargs='+', help='Names of the variables to extract')
+    parser.add_argument('band_number', type=int, help='Number of the band to extract (1-based)')
 
     args = parser.parse_args()
 
-    # Call the function for each variable
-    for variable_name in args.variable_names:
-        output_filename = f"{args.output_filename}_{variable_name}.tif"  # Append the variable name to the filename
-        grib_to_geotiff(args.input_filename, output_filename, variable_name)
+    # Call the function with the arguments
+    grib_to_geotiff(args.input_filename, args.output_filename, args.band_number)
 
