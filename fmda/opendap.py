438657c205ef68d38183d5e12f875a3a8a08e329
Jan Mandel <jan.mandel@gmail.com>
2023-12-22 10:28:55 -0700

    adding prints, need new url? modified:   opendap.py

--- fmda/opendap.py
+++ fmda/opendap.py
@@ -6,6 +6,7 @@ import xarray as xr
 url = 'https://nomads.ncep.noaa.gov:8080/dods/hrrr/hrrr20200910/hrrr_sfc_t18z'
 
 # Load the data using xarray
+print('xr.open_dataset',url)
 ds = xr.open_dataset(url)
 
 # Specify the point of interest
@@ -15,6 +16,7 @@ lat_point = 40.0  # latitude of the point of interest
 # Select variable and point of interest
 # Temperature is usually "tmp2m" for 2 meter temperature
 # Make sure to check the exact variable name in your dataset
+print('getting tmp2m at nearest to',lon_point,lat_point)
 temp_point = ds['tmp2m'].sel(lon=lon_point, lat=lat_point, method='nearest')
 
 # Print the time series for the selected point
