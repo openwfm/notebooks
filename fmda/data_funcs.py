bed715110201d489b407f887d4840bf4e10b82b4
jh-206 <jhirschi206@gmail.com>
2024-09-16 13:55:50 -0600

    Data Filters - PROCESS CODE
    Process code, doesn't all work yet

--- fmda/data_funcs.py
+++ fmda/data_funcs.py
@@ -14,6 +14,301 @@ import json
 import copy
 import subprocess
 import os.path as osp
+from utils import Dict, str2time, check_increment, time_intp
+
+def process_train_dict(input_file_path, data_params, atm_dict = "HRRR", verbose=False):
+    # Extract target and features
+    train = build_train_dict(input_file_path, atm=atm_dict, features_all=data_params['features_all'], verbose=verbose)
+    # Subset timeseries into shorter stretches
+    d = split_timeseries(train, hours=data_params['hours'], verbose=verbose)
+    d = discard_keys_with_short_y(d, hours=data_params['hours'], verbose=False)
+    # Check for suspect data
+    flags = flag_dict_keys(d, data_params['zero_lag_threshold'], data_params['max_intp_time'], 
+                       max_y = data_params['max_fm'], min_y = data_params['min_fm'], verbose=verbose)
+    # Remove flagged cases
+    cases = list([*d.keys()])
+    flagged_cases = [element for element, flag in zip(cases, flags) if flag == 1]
+    remove_key_list(d, flagged_cases, verbose=verbose)
+    return d
+
+feature_types = {
+    # Static features are based on physical location, e.g. location of RAWS site
+    'static': ['elev', 'lon', 'lat'],
+    # Atmospheric weather features come from either RAWS subdict or HRRR
+    'atm': ['temp', 'rh', 'wind', 'solar', 'soilm', 'canopyw', 'groundflux', 'Ed', 'Ew']
+}
+
+def build_train_dict(input_file_path,
+              forecast_step=1, atm="HRRR",features_all=['Ed', 'Ew', 'solar', 'wind', 'elev', 'lon', 'lat', 'rain'], verbose=False):
+    # in:
+    #   file_path       list of strings - files as in read_test_pkl
+    #   forecast_step   int - which forecast step to take atmospheric data from (maybe 03, must be >0). 
+    #   atm        str - name of subdict where atmospheric vars are located
+    #   features_list   list of strings - names of keys in subdicts to collect into features matrix. Default is everything collected
+    # return:
+    #   train          dictionary with structure
+    #                  {key : {'key' : key,    # copied subdict key
+    #                          'loc' : {...},  # copied from in dict = {key : {'loc': ... }...}
+    #                         'time' : time,   # datetime vector, spacing tres
+    #                            'X' : fm      # target fuel moisture from the RAWS, interpolated to time
+    #                            'Y' : feat    # features from atmosphere and location
+    #                            
+    #
+
+    
+    # TODO: fix this
+    if 'rain' in features_all and (not features_all[-1]=='rain'):
+        raise ValueError(f"Make rain in features list last element since (working on fix as of 24-6-24), given features list: {features_list}")
+    
+    if forecast_step > 0 and forecast_step < 100 and forecast_step == int(forecast_step):
+        fstep='f'+str(forecast_step).zfill(2)
+        fprev='f'+str(forecast_step-1).zfill(2)
+        # logging.info('Using data from step %s',fstep)
+        # logging.info('Using rain as the difference of accumulated precipitation between %s and %s',fstep,fprev)
+    else:
+        # logging.critical('forecast_step must be integer between 1 and 99')
+        raise ValueError('bad forecast_step')
+        
+    train = {}
+    with open(input_file_path, 'rb') as file:
+        # logging.info("loading file %s", file_path)
+        d = pickle.load(file)
+    for key in d:
+        atm_dict = atm
+        features_list = features_all
+        # logging.info('Processing subdictionary %s',key)
+        if key in train:
+            pass
+            # logging.warning('skipping duplicate key %s',key)
+        else:
+            subdict=d[key]    # subdictionary for this case
+            loc=subdict['loc']
+            train[key] = {
+            'id': key,  # store the key inside the dictionary, subdictionary will be used separatedly
+            'case':key,
+            'filename': input_file_path,
+            'loc': loc
+            }
+            desc='descr'
+            if desc in subdict:
+                train[desc]=subdict[desc]
+            time_hrrr=str2time(subdict[atm_dict]['time'])
+            # timekeeping
+            hours=len(d[key][atm_dict]['time'])
+            train[key]['hours']=hours
+            # train[key]['h2']   =hours     # not doing prediction yet    
+            hrrr_increment = check_increment(time_hrrr,id=key+f' {atm_dict}.time')
+            # logging.info(f'{atm_dict} increment is %s h',hrrr_increment)
+            if  hrrr_increment < 1:
+                # logging.critical('HRRR increment is %s h must be at least 1 h',hrrr_increment)
+                raise(ValueError)
+            
+            # build matrix of features - assuming all the same length, if not column_stack will fail
+            train[key]['time']=time_hrrr
+            # logging.info(f"Created feature matrix train[{key}]['X'] shape {train[key]['X'].shape}")
+            time_raws=str2time(subdict['RAWS']['time_raws']) # may not be the same as HRRR
+            # logging.info('%s RAWS.time_raws length is %s',key,len(time_raws))
+            check_increment(time_raws,id=key+' RAWS.time_raws')
+            # print_first(time_raws,num=5,id='RAWS.time_raws')
+            
+            # Set up static vars
+            columns=[]
+
+            missing_features = []
+            for feat in features_list:
+                # For atmospheric features,
+                if feat in feature_types['atm']:
+                    if atm_dict == "HRRR":
+                        vec = subdict['HRRR'][fstep][feat]
+                    elif atm_dict == "RAWS":
+                        if feat in subdict['RAWS'].keys():
+                            vec = time_intp(time_raws, subdict['RAWS'][feat], time_hrrr)
+                        else:
+                            missing_features.append(feat)
+                    columns.append(vec)
+                
+                # For static features, repeat to fit number of time observations
+                elif feat in feature_types['static']:
+                    columns.append(np.full(hours,loc[feat]))
+            
+            # compute rain as difference of accumulated precipitation
+            if 'rain' in features_list:
+                if atm_dict == "HRRR":
+                    rain = subdict[atm_dict][fstep]['precip_accum']- subdict[atm_dict][fprev]['precip_accum']
+                    # logging.info('%s rain as difference %s minus %s: min %s max %s',
+                             # key,fstep,fprev,np.min(rain),np.max(rain))
+                elif atm_dict == "RAWS":
+                    if 'rain' in subdict[atm_dict]:
+                        rain = time_intp(time_raws,subdict[atm_dict]['rain'],time_hrrr)
+                    else:
+                        pass
+                        # logging.info('No rain data found in RAWS subdictionary %s', key)
+                columns.append( rain ) # add rain feature         
+            else:
+                missing_features.append('rain')
+                
+            train[key]['X'] = np.column_stack(columns)
+            train[key]['features_list'] = [item for item in features_list if item not in missing_features]
+            
+            fm=subdict['RAWS']['fm']
+            # logging.info('%s RAWS.fm length is %s',key,len(fm))
+            # interpolate RAWS sensors to HRRR time and over NaNs
+            train[key]['y'] = time_intp(time_raws,fm,time_hrrr)
+            # TODO: check endpoint interpolation when RAWS data sparse, and bail out if not enough data
+            
+            if  train[key]['y'] is None:
+                pass
+                # logging.error('Cannot create target matrix for %s, using None',key)
+            else:
+                pass
+                # logging.info(f"Created target matrix train[{key}]['y'] shape {train[key]['y'].shape}")
+
+    # logging.info('Created a "train" dictionary with %s items',len(train))
+ 
+    # clean up
+    
+    keys_to_delete = []
+    for key in train:
+        if train[key]['X'] is None or train[key]['y'] is None:
+            # logging.warning('Deleting training item %s because features X or target Y are None', key)
+            keys_to_delete.append(key)
+
+    # Delete the items from the dictionary
+    if len(keys_to_delete)>0:
+        for key in keys_to_delete:
+            del train[key]       
+        # logging.warning('Deleted %s items with None for data. %s items remain in the training dictionary.',
+                        # len(keys_to_delete),len(train))
+        
+    # output
+
+    # if output_file_path is not None:
+    #     with open(output_file_path, 'wb') as file:
+    #         logging.info('Writing pickle dump of the dictionary train into file %s',output_file_path)
+    #         pickle.dump(train, file)
+    
+    # logging.info('pkl2train done')
+    
+    return train
+
+
+def remove_key_list(d, ls, verbose=False):
+    for key in ls:
+        if key in d:
+            if verbose:
+                print(f"Removing key {key} due to data flags")
+            del d[key]
+
+def split_timeseries(dict0, hours, naming_convention = "_set_", verbose=False):
+    """
+    Given number of hours, splits nested fmda dictionary into smaller stretches. This is used primarily to aid in filtering out and removing stretches of missing data.
+    """
+    cases = list([*dict0.keys()])
+    dict1={}
+    for key, data in dict0.items():
+        if verbose:
+            print(f"Processing case: {key}")
+            print(f"Length of y vector: {len(data['y'])}")
+        if type(data['time'][0]) == str:
+            time=str2time(data['time'])
+        else:
+            time=data['time']
+        X_array = data['X']
+        y_array = data['y']
+        
+        # Determine the start and end time for the 720-hour portions
+        start_time = time[0]
+        end_time = time[-1]
+        current_time = start_time
+        portion_index = 1
+        while current_time < end_time:
+            next_time = current_time + timedelta(hours=720)
+            
+            # Create a mask for the time range
+            mask = (time >= current_time) & (time < next_time)
+            
+            # Apply the mask to extract the portions
+            new_time = time[mask]
+            new_X = X_array[mask]
+            new_y = y_array[mask]
+            
+            # Save the portions in the new dictionary with naming convention if second or more portion
+            if portion_index > 1:
+                new_key = f"{key}{naming_convention}{portion_index}"
+            else:
+                new_key = key
+            dict1[new_key] = {'time': new_time, 'X': new_X, 'y': new_y}
+            
+            # Add other keys that aren't subset
+            for key2 in dict0[key].keys():
+                if key2 not in ['time', 'X', 'y']:
+                    dict1[new_key][key2] = dict0[key][key2]
+            
+            # Move to the next portion
+            current_time = next_time
+            portion_index += 1    
+        if verbose:
+            print(f"Partitions of length {hours} from case {key}: {portion_index-1}")
+    return dict1
+
+def flag_lag_stretches(x, threshold, lag = 1):
+    """
+    Used to itentify stretches of data that have been interpolated a length greater than or equal to given threshold. Used to identify stretches of data that are not trustworthy due to extensive interpolation and thus should be removed from a ML training set.
+    """
+    lags = np.round(np.diff(x, n=lag), 8)
+    zero_lag_indices = np.where(lags == 0)[0]
+    current_run_length = 1
+    for i in range(1, len(zero_lag_indices)):
+        if zero_lag_indices[i] == zero_lag_indices[i-1] + 1:
+            current_run_length += 1
+            if current_run_length > threshold:
+                return True
+        else:
+            current_run_length = 1
+    else:
+        return False   
+
+
+def flag_dict_keys(dict0, lag_1_threshold, lag_2_threshold, max_y, min_y, verbose=False):
+    """
+    Loop through dictionary and generate list of flags for if the `y` variable within the dictionary has target lag patterns. The lag_1_threshold parameter sets upper limit for the number of constant, zero-lag stretches in y. The lag_2_threshold parameter sets upper limit for the number of constant, linear stretches of data. Used to identify cases of data that have been interpolated excessively long and thus not trustworthy for inclusion in a ML framework.
+    """
+    cases = list([*dict0.keys()])
+    flags = np.zeros(len(cases))
+    for i, case in enumerate(cases):
+        if verbose:
+            print("~"*50)
+            print(f"Case: {case}")
+        y = dict0[case]['y']
+        if flag_lag_stretches(y, threshold=lag_1_threshold, lag=1):
+            if verbose:
+                print(f"Flagging case {case} for zero lag stretches greater than param {lag_1_threshold}")
+            flags[i]=1
+        if flag_lag_stretches(y, threshold=lag_2_threshold, lag=2):
+            if verbose:
+                print(f"Flagging case {case} for constant linear stretches greater than param {lag_2_threshold}")
+            flags[i]=1
+        if np.any(y>=max_y) or np.any(y<=min_y):
+            if verbose:
+                print(f"Flagging case {case} for FMC outside param range {min_y,max_y}. FMC range for {case}: {y.min(),y.max()}")
+            flags[i]=1    
+
+    return flags
+
+def discard_keys_with_short_y(input_dict, hours, verbose=False):
+    """
+    Remove keys from a dictionary where the subkey `y` is less than given hours. Used to remove partial sequences at the end of timeseries after the longer timeseries has been subdivided.
+    """
+    discarded_keys = [key for key, value in input_dict.items() if len(value['y']) < hours]
+    
+    if verbose:
+        print(f"Discarded keys due to y length less than {hours}: {discarded_keys}")
+    
+    filtered_dict = {key: value for key, value in input_dict.items() if key not in discarded_keys}
+    
+    return filtered_dict
+
+
 
 # Utility to combine nested fmda dictionaries
 def combine_nested(nested_input_dict, verbose=True):
