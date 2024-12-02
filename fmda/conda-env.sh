9408b218ef370419eec6f2f1fd2d6c7c2d0ad238
Jan Mandel <jan.mandel@gmail.com>
2024-12-01 19:49:52 -0700

    conda install order new file:   conda-env.sh

---
+++ fmda/conda-env.sh
@@ -0,0 +1,8 @@
+conda create -n fmda -c conda-forge tensorflow
+conda activate fmda  
+conda install -c conda-forge xarray -y
+conda install -c conda-forge jupyter -y
+conda install -c conda-forge dask -y
+conda install -c conda-forge netcdf4 -y
+conda install -c conda-forge sklearn -y
+
